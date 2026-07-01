61752050907c793c5f32de96ea9b96878a6f6ae0
Wassefdabbas <wassef.dabbas@gmail.com>
2026-07-02 01:09:54 +0300

    feat(ingestion): implement secure async file upload and decoupled controller architecture

---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class ResponseStatus(Enum):
+    SUCCESS = "success"
+    ERROR = "error"
+    WARNING = "warning"
+    FILE_TYPE_NOT_SUPPORTED = "File type is not allowed."
+    FILE_SIZE_EXCEEDS_LIMIT = "File size exceeds the maximum limit of MB."
+    FILE_UPLOAD_SUCCESS = "File uploaded successfully."
+    FILE_UPLOAD_FAILED = "File upload failed."
